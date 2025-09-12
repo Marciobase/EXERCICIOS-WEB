47868d1a7dd90ba026a11832b0c562c8b1c3e0a4
Marciobase <ignacio.marcio@gmail.com>
2025-09-12 16:27:52 -0300

    webpack - início

---
+++ webpack/src/principal.js
@@ -0,0 +1,5 @@
+import Pessoa from './pessoa'
+// const Pessoa = require('./pessoa')
+
+const marcio = new Pessoa
+console.log(marcio.cumprimentar())
---
+++ webpack/webpack.config.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack')
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+
+module.exports = {
+    // mode: 'development',
+    mode: 'production',
+    entry: './src/principal.js'
+}
