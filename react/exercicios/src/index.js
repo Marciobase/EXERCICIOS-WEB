4d126d8edd974b950093e2ce1d942b3de79635ba
Marciobase <ignacio.marcio@gmail.com>
2025-09-28 09:19:08 -0300

    React - criando primento componente

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
+
+import Primeiro from './components/firstComponent'
 
 
-ReactDOM.render(<h1>React</h1>, document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Primeiro />
+)
+
+
